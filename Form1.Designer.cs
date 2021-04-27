d01f1f7fb8fb4ce4fb06deeba6398b16c9f5c896
Kazi Safayat Huq <65438156+KaziHuq@users.noreply.github.com>
2021-04-27 14:55:51 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace MyFormOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.Location = new System.Drawing.Point(451, 123);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(97, 38);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Name:";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.txtBox1.Location = new System.Drawing.Point(595, 116);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(237, 45);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(640, 209);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(143, 54);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Submit";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl2.ForeColor = System.Drawing.Color.Yellow;
+            this.lbl2.Location = new System.Drawing.Point(567, 381);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 38);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1297, 623);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Name Retrieval App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl2;
+    }
+}
+
